c747af1cc76eb7bd2507b9c05fe1cd6c1d5b2fe1
LooreT <lorenna.turco@gmail.com>
2023-07-26 23:54:17 +0200

    Add Filter + Modal + Cat

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,8 @@
+
+console.info(localStorage.getItem("user"))
+
+
+
 fetch('http://localhost:5678/api/works')
     .then(function (response) {
         if (response.ok) {
@@ -17,11 +22,12 @@ fetch('http://localhost:5678/api/works')
             })
             .then(function (resultCat) {
                 console.log(resultCat)
-
+                addWorkModal(resultWork);
                 let filterList = document.getElementById("filtre");
                 let buttonFilter = document.createElement("button");
                 buttonFilter.setAttribute("class", "buttonFilterFull");
                 buttonFilter.innerHTML = "Tous";
+
                 // AJOUT POUR TOUS
                 buttonFilter.addEventListener("click", () => {
                     let buttonToReset = document.querySelector("#filtre .buttonFilterFull");
@@ -29,9 +35,8 @@ fetch('http://localhost:5678/api/works')
                     buttonFilter.setAttribute("class", "buttonFilterFull");
                     document.querySelectorAll(".gallery figure").forEach((elem) => elem.remove());
                     showWork(resultWork);
-
-
                 });
+
                 filterList.appendChild(buttonFilter);
 
                 for (let x = 0; x < resultCat.length; x++) {
@@ -40,12 +45,11 @@ fetch('http://localhost:5678/api/works')
                     buttonFilter.innerHTML = resultCat[x].name;
                     filterList.appendChild(buttonFilter);
 
-                    // EVENT 
+                    // EVENT CATEGORIES
                     buttonFilter.addEventListener("click", () => {
                         let buttonToReset = document.querySelector("#filtre .buttonFilterFull");
                         buttonToReset.setAttribute("class", "buttonFilter");
                         buttonFilter.setAttribute("class", "buttonFilterFull");
-
 
                         const workFiltered = resultWork.filter(function (work) {
                             // Identique ? booleen true false
@@ -63,6 +67,47 @@ fetch('http://localhost:5678/api/works')
 
             });
     });
+window.onload = function () {
+    let elementOutModal = document.getElementById("backgroundModal");
+    let elementModal = document.getElementById("containerModal");
+    elementOutModal.addEventListener("click", () => closeModal());
+    elementModal.addEventListener("click", (event) => event.stopPropagation())
+
+    let xMark = document.querySelector(".fa-xmark");
+    xMark.addEventListener("click", () => closeModal());
+
+    let isConnected = localStorage.getItem("user") != null;
+    let buttonModif = document.getElementById("editProject");
+
+    if (isConnected) {
+        buttonModif.setAttribute("class", "visible");
+    }
+
+    buttonModif.addEventListener("click", () => {
+        let modal = document.getElementById("backgroundModal");
+        modal.setAttribute("class", "background");
+    })
+
+    let buttonAdd = document.getElementById("addButton");
+    buttonAdd.addEventListener("click", () => {
+        let content1 = document.getElementById("content1");
+        content1.setAttribute("class", "no-display");
+        let arrowLeft = document.getElementById("arrowLeft");
+        arrowLeft.setAttribute("class", "fa-solid fa-arrow-left");
+        let content2 = document.getElementById("content2");
+        content2.setAttribute("class", "content-visible");
+    })
+
+    arrowLeft.addEventListener("click", () => {
+        let content1 = document.getElementById("content1");
+        content1.setAttribute("class", "content-visible");
+        let arrowLeft = document.getElementById("arrowLeft");
+        arrowLeft.setAttribute("class", "no-display");
+        let content2 = document.getElementById("content2");
+        content2.setAttribute("class", "no-display");
+    })
+}
+
 
 function showWork(listToShow) {
     let galleryPortfolio = document.querySelector("#portfolio .gallery");
@@ -84,3 +129,38 @@ function showWork(listToShow) {
         galleryPortfolio.appendChild(newFigure);
     }
 }
+
+function addWorkModal(listToShow) {
+    let galleryModal = document.querySelector("#galleryModal");
+
+    for (let x = 0; x < listToShow.length; x++) {
+
+        let newFigure = document.createElement("figure");
+        let newImg = document.createElement("img");
+        let newCaption = document.createElement("figcaption");
+        // image + alt
+        newImg.setAttribute("src", listToShow[x].imageUrl);
+        newImg.setAttribute("alt", listToShow[x].title);
+
+        // title caption
+        newCaption.innerHTML = "editer";
+
+        newFigure.appendChild(newImg);
+        newFigure.appendChild(newCaption);
+        galleryModal.appendChild(newFigure);
+    }
+}
+
+
+function closeModal() {
+    let elementModal = document.getElementById("backgroundModal");
+    elementModal.setAttribute("class", "no-display");
+    let content1 = document.getElementById("content1");
+    content1.setAttribute("class", "content-visible");
+    let content2 = document.getElementById("content2");
+    content2.setAttribute("class", "no-display");
+    let arrowLeft = document.getElementById("arrowLeft");
+    arrowLeft.setAttribute("class", "no-display");
+
+}
+
--- FrontEnd/scriptLogin.js
+++ FrontEnd/scriptLogin.js
@@ -1,34 +1,46 @@
-window.onload = function() {
-    let buttonSubmit = document.getElementById("formLogIn");
-    buttonSubmit.addEventListener("submit", () => {
-        let userMail = document.getElementById("mail")
-        let userMdp = document.getElementById("mdp")
+window.onload = function () {
+  let formLogIn = document.getElementById("formLogIn");
+  formLogIn.addEventListener("submit", (event) => {
+    // enlever rafraichissement automatique de la page
+    event.preventDefault();
+    let userMail = document.getElementById("mail")
+    let userMdp = document.getElementById("mdp")
     console.log(userMail.value)
     console.log(userMdp.value)
-    
-        let user = {
-            email: userMail.value,
-            password: userMdp.value 
-          }
-    
-        fetch('http://localhost:5678/api/users/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(user)
-          })
-          .then(function(response) {
-            return response.json()}   
-            )
-        .then(function (resultatPost) {
-            console.log(resultatPost)
 
+    let user = {
+      email: userMail.value,
+      password: userMdp.value
+    }
 
-
-
-            window.location.href = "C:/Users/Lore-/Desktop/PRO/OPENCLASSROOMS/P6_Portfolio-architecte-sophie-bluel-master/FrontEnd/index.html";
-        })
-
+    fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(user)
     })
+      .then(function (response) {
+        return response.json()
+      }
+      )
+      .then(function (resultatPost) {
+        console.log(resultatPost)
+
+        if(resultatPost.userId != null) { 
+        localStorage.setItem("user", resultatPost.token);
+        console.info(localStorage.getItem("user"))
+        window.location.href = "C:/Users/Lore-/Desktop/PRO/OPENCLASSROOMS/PROJET%206/FrontEnd/index.html";
+        }
+        else{
+          let errorMessage = document.getElementById("error");
+          errorMessage.setAttribute("class", "errorOn");
+        }
+
+
+
+        
+      })
+
+  })
 };
